e5116f2f9197515ec30875beec2950ad0238a45a
harmlessprince <realolamilekan@gmail>
2022-01-09 09:53:26 +0100

    updated users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
+            $table->string('social_id')->nullable();
+            $table->string('social_type')->nullable();
+            $table->string('name')->nullable();
             $table->string('username')->unique()->nullable();
             $table->string('phone')->unique()->nullable();
             $table->string('email')->unique();
@@ -28,7 +31,7 @@ class CreateUsersTable extends Migration
             $table->string('device_name')->nullable();
             $table->integer('status')->default(UserStatus::ACTIVE);
             //spatial
-            $table->ipAddress('ip')->nullable();
+            $table->string('ip')->nullable();
             $table->decimal('latitude', 11,8)->nullable();
             $table->decimal('longitude', 11, 8)->nullable();
             $table->string('country')->nullable()->index();
