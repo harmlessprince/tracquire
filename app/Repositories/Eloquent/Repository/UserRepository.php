617d543b47b4fbb704c335c27f5b68e6a743aa91
harmlessprince <realolamilekan@gmail>
2022-03-05 22:30:51 +0100

    swap can now be completed using username

--- app/Repositories/Eloquent/Repository/UserRepository.php
+++ app/Repositories/Eloquent/Repository/UserRepository.php
@@ -1,15 +1,22 @@
 <?php
 
 namespace App\Repositories\Eloquent\Repository;
+
 use App\Models\User;
+
 class UserRepository extends BaseRepository
 {
     public function __construct(User $model)
     {
         parent::__construct($model);
     }
-    public function creditWallet($user_id){
+    public function creditWallet($user_id)
+    {
         dd($user_id);
     }
-}
 
+    public function findByUserName(string $username)
+    {
+        return $this->model::where('username', $username)->first();
+    }
+}
