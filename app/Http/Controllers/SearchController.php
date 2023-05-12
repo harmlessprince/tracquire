ab2d142496d4ecaaff3197105b55b76263a8d5a2
harmlessprince <realolamilekan@gmail.com>
2023-05-12 07:55:45 +0100

    fixed search filter

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Enums\PostStatus;
 use App\Http\Resources\Post\PostCollection;
 use App\Models\Post;
+use Illuminate\Http\Response;
 use Spatie\QueryBuilder\QueryBuilder;
 /**
  * @group Search Post
@@ -24,9 +25,9 @@ class SearchController extends Controller
      * @apiResourceCollection App\Http\Resources\Post\PostCollection
      * @apiResourceModel App\Models\Post
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
-    public function index()
+    public function index(): Response
     {
         $posts = QueryBuilder::for(Post::class)
             ->allowedFilters(['description', 'condition', 'title', 'location'])
